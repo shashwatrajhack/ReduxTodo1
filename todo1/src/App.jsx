c0dcf2531c872d52976d45cce12e32bc63de8719
shashwatrajhack <er.shashwatraj@gmail.com>
2025-11-17 18:30:08 +0530

    todoList

---
+++ todo1/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Home from './components/Home'
+
+function App() {
+
+  return (
+    <>
+     <h1>Todo using Redux</h1>
+     <Home />
+    </>
+  )
+}
+
+export default App
---
+++ todo1/src/components/Home.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { useState } from "react";
+
+function Home() {
+  const [inputValue, setInputValue] = useState("");
+  const [todo, setTodo] = useState([]);
+  const [id, setId] = useState();
+  const [toggle, setToggle] = useState(false);
+
+  const addFunction = () => {
+    if (toggle) {
+      const todo2 = [...todo];
+      todo2[id] = inputValue;
+      setTodo(todo2);
+      setToggle(false);
+      return
+    }
+
+    setTodo([...todo, inputValue]);
+    setInputValue("");
+  };
+
+  const deleteFunction = (i) => {
+    const filterTodo = todo.filter((x) => x !== i);
+    console.log(filterTodo);
+
+    setTodo([...filterTodo]);
+  };
+
+  const handleUp = (i) => {
+    // [todo[i],todo[i-1]] = [todo[i-1],todo[i]];
+    const todo2 = [...todo];
+    let temp = todo2[i];
+    todo2[i] = todo2[i - 1];
+    todo2[i - 1] = temp;
+
+    setTodo([...todo2]);
+  };
+
+  const handleDown = (i) => {
+    const todo2 = [...todo];
+    let temp = todo2[i];
+    todo2[i] = todo2[i + 1];
+    todo2[i + 1] = temp;
+
+    setTodo([...todo2]);
+  };
+
+  const editTodo = (i) => {
+    setId(i);
+    setInputValue(todo[i]);
+    setToggle(true);
+  };
+
+  return (
+    <div>
+      <input
+        value={inputValue}
+        placeholder="add todo"
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={addFunction} disabled={!inputValue.trim()}>
+        {toggle ? "Update" : "Add Todo"}
+      </button>
+      <div>
+        {todo.map((x, i) => (
+          <li>
+            <h3>
+              {x} <button onClick={() => editTodo(i)}>edit</button>
+              <button onClick={() => deleteFunction(x)}>delete</button>
+              <button onClick={() => handleUp(i)}>up</button>
+              <button onClick={() => handleDown(i)}>down</button>
+            </h3>
+          </li>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
