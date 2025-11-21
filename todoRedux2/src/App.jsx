f1b24f578ea30a188630bb3e40cafe571de54d74
shashwatrajhack <er.shashwatraj@gmail.com>
2025-11-21 08:06:49 +0530

    todo app

--- todoRedux2/src/App.jsx
+++ todoRedux2/src/App.jsx
@@ -1,12 +1,30 @@
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
-import { updateInputValue, addTodo } from "./features/todo/todoSlice";
-
+import {
+  updateInputValue,
+  addTodo,
+  removeTodo,
+  editTodo,
+  updateFilteredTodo,
+  searchInputFunction,
+  markDone,
+  dropDown,
+  clearTodo,
+} from "./features/todo/todoSlice";
+import { useEffect } from "react";
 function App() {
+  //const [filter, setFilter] = useState("");
   const selector = useSelector((state) => state.inputValue);
   const todos = useSelector((state) => state.todos);
+  const filteredTodo = useSelector((state) => state.filteredTodo);
+  const selectDropDown = useSelector((state) => state.selectDropDown);
+  const id = useSelector((state) => state.id);
+  const searchTodo = useSelector((state) => state.searchInput);
   const dispatch = useDispatch();
-  console.log(todos);
+  useEffect(() => {
+    localStorage.setItem("todoData", JSON.stringify(todos));
+    dispatch(updateFilteredTodo());
+  }, [todos]);
 
   return (
     <>
@@ -18,13 +36,58 @@ function App() {
           value={selector}
           onChange={(e) => dispatch(updateInputValue(e.target.value))}
         />
-        <button onClick={() => dispatch(addTodo())}>Add</button>
+        <button onClick={() => dispatch(addTodo())}>
+          {id != -1 ? "update" : "Add"}
+        </button>
       </div>
       <div>
-        {todos.map((x) => {
-          return <li>{x}</li>;
+        <input
+          placeholder="search todo"
+          value={searchTodo}
+          onChange={(e) => dispatch(searchInputFunction(e.target.value))}
+        />
+
+        <select
+          value={selectDropDown}
+          onChange={(e) => dispatch(dropDown(e.target.value))}
+        >
+          <option value="all">All</option>
+          <option value="true">Completed</option>
+          <option value="false">Incomplete</option>
+        </select>
+        <button onClick={() => dispatch(clearTodo())}>Clear All</button>
+      </div>
+      <div>
+        {filteredTodo.map((x, i) => {
+          return (
+            <li key={i}>
+              <span className={x.status ? "text-decoration-message" : ""}>
+                {x.text}
+              </span>
+
+              <button
+                onClick={() => dispatch(removeTodo(x.ids))}
+                disabled={x.status}
+                className={x.status ? "text-decoration-message" : ""}
+              >
+                delete
+              </button>
+              <button
+                onClick={() => dispatch(editTodo(x.ids))}
+                disabled={x.status}
+                className={x.status ? "text-decoration-message" : ""}
+              >
+                edit
+              </button>
+
+              <button onClick={() => dispatch(markDone(x.ids))}>
+                mark as done
+              </button>
+            </li>
+          );
         })}
       </div>
+      <h3>Total number of task : {todos.length}</h3>
     </>
   );
 }
--- todoRedux2/src/features/todo/todoSlice.js
+++ todoRedux2/src/features/todo/todoSlice.js
@@ -1,27 +1,33 @@
 /*
-    1.  Add new task
+  1.  Add new task
 	2.	Edit task
 	3.	Delete task
 	4.	Mark task as completed / uncompleted
-	5.	Filter tasks → All / Active / Completed
+	5.	Filter tasks dropdown -  / Completed
 	6.	Clear all completed tasks
 	7.	Task counter (e.g., “3 tasks left”)
 	8.	Save tasks in localStorage
 	9.	Responsive design (mobile-friendly)
-    10. Search bar to find tasks
-    11. Sub-tasks (nested todos)
-    12. Dark / light mode toggle
+  10. Search bar to find tasks
+  11. Sub-tasks (nested todos)
+  12. Dark / light mode toggle
 	13. Confirm dialog before delete
 	14. Toast messages for actions (added, deleted, etc.)
 	15. Auto-save feature (only when typing in edit mode)
-    16. Keyboard shortcuts (Enter to add, Esc to cancel)
+  16. Keyboard shortcuts (Enter to add, Esc to cancel)
 */
 
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-  todos: [],
+  todos: localStorage.getItem("todoData")
+    ? JSON.parse(localStorage.getItem("todoData"))
+    : [],
+  filteredTodo: [],
   inputValue: "",
+  searchInput: "",
+  id: -1,
+  selectDropDown: "all",
 };
 
 export const todoSlice = createSlice({
@@ -29,16 +35,89 @@ export const todoSlice = createSlice({
   initialState,
   reducers: {
     addTodo: (state) => {
-      state.todos.push(state.inputValue);
-      id: nanoid, (state.inputValue = "");
+      let existingValue = state.todos.find(
+        (value) => state.inputValue.toLowerCase() == value.text.toLowerCase()
+      );
+
+      if (existingValue) {
+        return alert("already exist");
+      }
+
+      if (state.id != -1) {
+        // for (let i = 0; i < state.todos.length; i++) {
+        //   if (state.id == state.todos[i].ids) {
+        //     state.todos[i].text = state.inputValue;
+        //   }
+        // }
+        let foundIndex = state.todos.findIndex((val) => val.ids === state.id);
+        if (foundIndex !== -1) {
+          state.todos[foundIndex].text = state.inputValue;
+        }
+        state.id = -1;
+        state.inputValue = "";
+        return;
+      }
+      let obj = { ids: nanoid(), text: state.inputValue, status: false };
+      state.todos.push(obj);
+      state.inputValue = "";
     },
-    removeTodo: (state, action) => {},
+    removeTodo: (state, action) => {
+      let index = action.payload;
+      state.todos = state.todos.filter((data) => data.ids !== index);
+    },
+    editTodo: (state, action) => {
+      state.id = action.payload;
+      state.inputValue = state.todos.find((i) => i.ids == state.id).text;
+      console.log(state.todos.find((x) => x.ids == state.id).text);
+    },
     updateInputValue: (state, action) => {
       state.inputValue = action.payload;
+    },
+    updateFilteredTodo: (state) => {
+      state.filteredTodo = state.todos;
+    },
+    searchInputFunction: (state, action) => {
+      state.searchInput = action.payload;
+      state.filteredTodo = state.todos.filter((x) =>
+        x.text.toLowerCase().includes(state.searchInput.toLowerCase())
+      );
+    },
+    markDone: (state, action) => {
+      let foundIndex = state.todos.findIndex(
+        (val) => val.ids === action.payload
+      );
+      if (foundIndex !== -1) {
+        state.todos[foundIndex].status = !state.todos[foundIndex].status;
+      }
+      console.log(state.status);
+    },
+    dropDown: (state, action) => {
+      state.selectDropDown = action.payload;
+      console.log(state.selectDropDown);
+      if (state.selectDropDown == "true") {
+        state.filteredTodo = state.todos.filter((x) => x.status == true);
+      } else if (state.selectDropDown == "false") {
+        state.filteredTodo = state.todos.filter((x) => x.status == false);
+      } else {
+        state.filteredTodo = state.todos;
+      }
+    },
+    clearTodo: (state) => {
+      state.filteredTodo = state.todos.filter((x) => x.status != true);
     },
   },
 });
 
-export const { addTodo, removeTodo, updateInputValue } = todoSlice.actions;
+export const {
+  addTodo,
+  removeTodo,
+  updateInputValue,
+  editTodo,
+  updateFilteredTodo,
+  searchInputFunction,
+  markDone,
+  dropDown,
+  clearTodo,
+} = todoSlice.actions;
 
 export default todoSlice.reducer;
